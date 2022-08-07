53d683d8e3fe54d03526f2a2f861843151adc401
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-07 05:38:57 -0500

    Agregando estilos a Listado de pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,28 @@
+
+export default function Paciente() {
+  return (
+    <div className="m-5 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Yonk</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Alex</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">alexnear@gmail.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">10 Octubre de 1990</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+                <span className="font-normal normal-case">Lorem ipsum dolor, sit amet consectetur 
+                adipisicing elit. Officiis, voluptate. Quos, ut soluta blanditiis ullam magni modi ab fugit quasi. 
+                Pariatur deserunt corporis architecto. Est tenetur cum omnis quisquam exercitationem.</span>
+            </p>
+        </div>
+  )
+}

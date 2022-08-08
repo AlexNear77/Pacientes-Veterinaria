66b6c67011d0c5415aac3b5d936d751031a98abb
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-08 06:11:38 -0500

    Agregando funcionalidad al componente ListadoPacientes y añadiendo estilos y agregando botones de gestion

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -21,6 +21,18 @@ export default function Paciente({paciente}) {
             <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
                 <span className="font-normal normal-case">{paciente.sintomas}</span>
             </p>
+
+            <div className="flex justify-between mt-5">
+                <button type="button" 
+                        className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg" >
+                    Editar
+                </button>
+
+                <button type="button"
+                        className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg" >
+                    Eliminar
+                </button>
+            </div>
         </div>
   )
 }

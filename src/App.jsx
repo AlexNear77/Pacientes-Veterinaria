1addabc42672d102b626e418772d53c560eeb135
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-10 19:48:31 -0500

    Teminando funcionalidad del boton Eliminar, en componente Paciente

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
   const [pacientes , setPacientes] = useState([]);
   const [paciente , setPaciente ] = useState({});
 
+  const eliminarPaciente = (id) => {
+      const pacientesActualizados = pacientes.filter(pacientito => {
+        return pacientito.id !== id
+      })
+      setPacientes(pacientesActualizados);
+  }
+
   return (
     
     <div className="container mx-auto mt-20">
@@ -23,6 +30,7 @@ function App() {
           <ListadoPacientes 
             pacientes={pacientes}
             setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
           />
         </div>
     </div>
